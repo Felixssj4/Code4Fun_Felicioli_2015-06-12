71c37287e18dbca38c97026a820ed586bba12d54
Felixssj4 <danilo.felicioli@hotmail.it>
2015-06-12 23:08:01 +0200

    .

--- Exercise2/Exercise2/Program.cs
+++ Exercise2/Exercise2/Program.cs
@@ -10,16 +10,44 @@ namespace Exercise2
     {
         static void Main(string[] args)
         {
+            List<int> l = new List<int>() { 1, 2, 3, 4, 5 };
+            
+            LinkedList<int> ll = new LinkedList<int>(l);
+
+            try 
+            { 
+                Console.Write(GetNthElemntOfList(0, ll).ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+                
+            Console.ReadKey();
         }
 
         public static T GetNthElemntOfList<T>(int N, LinkedList<T> lista)
         {
             LinkedListNode<T> n1 = lista.First;
             LinkedListNode<T> n2 = lista.First;
+            int count = 1;
 
-            n2 = lista.
+            if (N <= 0)
+                throw new Exception("Il numero dell'elemento richiesto deve essere maggiore di 0 ");
 
-            return lista.First.Value;
+            while (n2.Next != null)
+            {
+                count++;
+                if (count > N)
+                    n1 = n1.Next;
+                n2 = n2.Next;
+            }
+
+            if (count < N)
+                throw new Exception("La lista non contiene " + N + " elementi.");
+
+
+            return n1.Value;
         }
     }
 }
