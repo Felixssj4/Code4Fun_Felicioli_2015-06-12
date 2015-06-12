190e8d97d4657c3946e87905e5503d8b282ae424
Felixssj4 <danilo.felicioli@hotmail.it>
2015-06-12 22:34:18 +0200

    Last commit

--- Exercise1/Exercise1/Program.cs
+++ Exercise1/Exercise1/Program.cs
@@ -10,6 +10,38 @@ namespace Exercise1
     {
         static void Main(string[] args)
         {
+            int n = 25;
+            int[] arr = RandomArray(n);
+            for (int x = 0; x < n; x++)
+                Console.Write(arr[x] + " ");
+
+            Console.ReadKey();
+        }
+
+        public static int[] RandomArray(int N)
+        {
+            int[] array = new int[N];
+
+            List<int> app = new List<int>();
+
+            for (int x = 1; x <= N; x++)
+            {
+                app.Add(x);
+            }
+
+            Random rnd = new Random();
+            int rndIndex = 0;
+            int index = 0;
+
+            while (app.Count > 0)
+            {
+                rndIndex = rnd.Next(0, app.Count() - 1);
+                array[index] = app[rndIndex];
+                app.RemoveAt(rndIndex);
+                index++;
+            }
+
+            return array;
         }
     }
 }
