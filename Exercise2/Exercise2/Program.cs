46a778b2c73fbf53a76d20221fb4ccb6c0a4e528
Felixssj4 <danilo.felicioli@hotmail.it>
2015-06-12 22:36:22 +0200

    .

--- Exercise2/Exercise2/Program.cs
+++ Exercise2/Exercise2/Program.cs
@@ -11,5 +11,15 @@ namespace Exercise2
         static void Main(string[] args)
         {
         }
+
+        public static T GetNthElemntOfList<T>(int N, LinkedList<T> lista)
+        {
+            LinkedListNode<T> n1 = lista.First;
+            LinkedListNode<T> n2 = lista.First;
+
+            n2 = lista.
+
+            return lista.First.Value;
+        }
     }
 }
